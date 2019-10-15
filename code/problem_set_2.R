e7dbd07eddcfde939a09afcacfef9e00080ff978
John <55668297+Duong-Nguyen73@users.noreply.github.com>
2019-10-15 00:10:10 -0700

    rework #2

--- code/problem_set_2.R
+++ code/problem_set_2.R
@@ -4,25 +4,32 @@
 library(tidyverse)
 library(haven)
 library(tidyr)
+library(dbplyr)
 
 # call-up data #
 nfhs <- read_dta("./raw_data/IAHR52FL.dta")
 
-# create new dataframe/subset #
-# Q1 goes here #
+# Q1 #
+# answer: household #
 
 # Q2 #
 # hv208-has television  hv270-wealth #
-hh_wealth <- select(nfhs, hhid:hv208, hv270)
+new_df <- select(nfhs, hhid:hv208, hv270)
+# answer: 48 #
 
 # Q3 #
 # ind.line.no.-hv003 edu.sin.yr.-hv108,v133 #
-hh_edu <- select(nfhs, hhid, contains("hv108"))
+hh_edu <- select(nfhs, hhid, hv003, contains("hv108")) # WRONG! # # ?hvidx? #
+hh_edu1 <- select(nfhs, hhid, starts_with("hvidx"), contains("hv108"))
+# answer: 71 #
 
+# Q4 #
 female_bio <- select(nfhs, hhid, matches("ha\\d_\\d+"))
+# answer: 78 #
 
 # Q5 #
 male_bio <- select(nfhs, hhid, matches("hb\\d_\\d+"))
+# answer: 127 #
 
 # Q6 #
 hh_gat <- hh_edu %>% gather(key = "all_variables", value = "years", -hhid)
@@ -44,7 +51,17 @@ fem_fdf <- fem_spr %>%
   rename('Age' = ha1_, 'Weight' = ha2_, 'Height' = ha3_)
 
 # 8 #
+male_gat <- male_bio %>% 
+  select(c(hhid, matches('hb[1-3]'))) %>% 
+  gather(matches('hb[1-3]'),
+  key = 'all variables', value = 'yrs, lbs, feetinches', na.rm = TRUE)
+male_sep <- male_gat %>% separate('all variables',
+  into = c('Age Weight Height', 'Family Members', ' Years of Education'))
+male_spr <- male_sep %>% 
+  spread(key = 'Age Weight Height', value = 'yrs, lbs, feetinches')
+male_fdf <- male_spr %>%
+  rename('Age' = hb1_, 'Weight' = hb2_, 'Height' = hb3_)
 
+# 9 #
 
-t
 
